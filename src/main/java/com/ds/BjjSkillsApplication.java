2c9425271e64e914609fe799f8e216006be3905b
Dropsu <stube01@gmail.com>
2016-09-24 13:12:32 +0200

    initial commit

---
+++ src/main/java/com/ds/BjjSkillsApplication.java
@@ -0,0 +1,12 @@
+package com.ds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BjjSkillsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BjjSkillsApplication.class, args);
+	}
+}
