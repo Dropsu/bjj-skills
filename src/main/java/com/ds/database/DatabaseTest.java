bc9264698318e3b20bdc7bb9b875259130a2b29b
Dropsu <stube01@gmail.com>
2016-09-28 18:22:46 +0200

    another db change

--- src/main/java/com/ds/database/DatabaseTest.java
+++ src/main/java/com/ds/database/DatabaseTest.java
@@ -3,7 +3,9 @@ package com.ds.database;
 import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DatabaseTest {
 
@@ -11,6 +13,20 @@ public class DatabaseTest {
 	    String dbUrl = System.getenv("JDBC_DATABASE_URL");
 	    return DriverManager.getConnection(dbUrl);
 	}
+	
+	  public static void dbTaste() throws Exception {
+	        
+	        Connection connection = getConnection();
+	        
+	        Statement stmt = connection.createStatement();
+	        stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
+	        stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
+	        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+	        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+	        while (rs.next()) {
+	            System.out.println("Read from DB: " + rs.getTimestamp("tick"));
+	        }
+	    }
 	
 	public static String sayHolloFromDb () {
 		return "hollo from db";
