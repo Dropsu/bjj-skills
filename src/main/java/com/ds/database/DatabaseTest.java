850b58c429973de1153dde1553ee309a8b401d80
Dropsu <stube01@gmail.com>
2016-09-29 17:03:34 +0200

    db connection for remote and local

--- src/main/java/com/ds/database/DatabaseTest.java
+++ src/main/java/com/ds/database/DatabaseTest.java
@@ -1,6 +1,8 @@
 package com.ds.database;
 
+import java.net.URI;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import com.heroku.sdk.jdbc.DatabaseUrl;
@@ -12,8 +14,18 @@ public class DatabaseTest {
 	      
 		  	String result = "";
 		  
-	        Connection  connection = DatabaseUrl.extract().getConnection();
+//	        Connection  connection = DatabaseUrl.extract().getConnection();
 	        
+		  	
+		  	
+		  	URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+		    String username = dbUri.getUserInfo().split(":")[0];
+		    String password = dbUri.getUserInfo().split(":")[1];
+		    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+		    Connection connection = DriverManager.getConnection(dbUrl, username, password);
+		  	
 	        Statement stmt = connection.createStatement();
 	        stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
 	        stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
---
+++ src/main/java/com/ds/domain/Technique.java
@@ -0,0 +1,17 @@
+package com.ds.domain;
+
+import java.util.ArrayList;
+
+public class Technique {
+
+	private String name;
+	private String desc;
+	private ArrayList <String> links;
+	private String imgLink;
+	private String startingPos;
+	private String finalPos;
+	private byte lvlOfCompetence;
+	
+	
+	
+}
